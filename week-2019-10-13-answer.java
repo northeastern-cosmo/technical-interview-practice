cbf8319ae221fd5032c433527a74d95679f303b5
unknown <19chrisg97@gmail.com>
2019-10-19 16:08:43 -0400

    added answer for last week

---
+++ week-2019-10-13-answer.java
@@ -0,0 +1,10 @@
+int countFactZeros(int num) { 
+    int count = 0; 
+    if (num < 0) { 
+        return -1; 
+    }
+    for (int i= 5; num / i > 0; i *= 5) { 
+        count+= num / i;
+    }
+    return count;
+} 
